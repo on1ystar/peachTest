9085e31cbdff7622113cff4ea79c83427e61f6e7
on1ystar <tjdwls0607@naver.com>
2021-07-03 02:12:17 +0900

    s3 setup

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Welcome to peach !'));  
+app.get('/', (req, res) => res.send('Welcome to peach !'));
 
 app.listen(PORT, () =>
-  console.log(`✅ Server listening on test.k-peach.io:${PORT}/`)
+  console.log(`✅ Server listening on api.k-peach.io:${PORT}/`)
 );
