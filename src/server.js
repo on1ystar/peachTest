d51442312a5e358337d686fec443fd945b83485d
on1ystar <tjdwls0607@naver.com>
2021-07-05 12:16:50 +0000

    swagger ui setting

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 import helmet from 'helmet';
 import express from 'express';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 import rootRouter from './routers/rootRouter';
 
 const app = express();
 const logger = morgan('dev');
+const swaggerSpec = YAML.load(path.join(__dirname, 'swagger/swagger.yaml'));
 
 app.use(helmet());
 app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/', rootRouter);
 
--- src/test.js
+++
@@ -1,4 +0,0 @@
-console.log('hi');
-console.log('hi');
-console.log('hi');
-console.log('hi');
